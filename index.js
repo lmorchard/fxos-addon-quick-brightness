a14b4ac88d67d18e3aa7669e5059c8df96fbf650
Les Orchard <me@lmorchard.com>
2015-06-30 01:31:16 -0400

    Tweak styles a bit; add my URL to manifest

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 (function () {
 
+// Just a small shortcut to repeat myself less
+var $$ = document.getElementById.bind(document);
+
 // Remove existing control, for when this addon is re-run.
-var existingContainerEl = document.getElementById('quick-brightness-container');
+var existingContainerEl = $$('quick-brightness-container');
 if (existingContainerEl) {
   existingContainerEl.parentNode.removeChild(existingContainerEl);
 }
@@ -14,8 +17,8 @@ containerEl.setAttribute('data-time-inserted', Date.now());
 // Inline styles are icky, but I think injected addon CSS is broken right now.
 // see: https://developer.mozilla.org/en-US/Firefox_OS/Add-ons#Stylesheets
 containerEl.setAttribute('style', [
-  'height: 3rem;',
-  'margin: 0.25rem;'
+  'height: 3.5rem;',
+  'margin: 1.5rem 0 0 0;'
 ].join('\n'));
 
 // Markup stolen and munged from gaia settings
@@ -32,8 +35,8 @@ containerEl.innerHTML = [
 ].join('\n');
 
 // Inject the elements into the system app
-var trayEl = document.getElementById('utility-tray-footer');
-var quickSettings = document.getElementById('quick-settings');
+var trayEl = $$('utility-tray-footer');
+var quickSettings = $$('quick-settings');
 trayEl.insertBefore(containerEl, quickSettings);
 
 // Borrow some code from Gaia shared/js/settings_listener.js
@@ -45,7 +48,7 @@ function sl_getSettingsLock() {
 }
 
 // Wire up an event listener to set brightness on slider change.
-var sliderEl = document.getElementById('quick-brightness-control');
+var sliderEl = $$('quick-brightness-control');
 sliderEl.addEventListener('change', function (ev) {
   sl_getSettingsLock().set({
     'screen.brightness': sliderEl.value
